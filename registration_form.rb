3c7aa53f16627ba33757bd6a24698332de6aa493
Apple Man <happy3guy@gmail.com>
2023-07-07 19:08:28 +0300

    feat: scrapping & driving

---
+++ registration_form.rb
@@ -0,0 +1,19 @@
+require 'capybara/dsl'
+
+include Capybara::DSL
+
+Capybara.current_driver = Capybara.javascript_driver
+Capybara.app_host = 'http://techcareerbooster-fake-data.herokuapp.com'
+
+visit '/exercises_and_code/students/new'
+
+fill_in 'Name', with: 'John'
+fill_in 'Email', with: 'john@mailinator.com'
+fill_in 'Street number', with: '46232 Connelly Neck'
+fill_in 'City', with: 'North Maudie'
+fill_in 'Zip code', with: '62178'
+select 'Bolivia', from: 'Country'
+
+click_button 'Create Student'
+
+find('.alert-success', text: 'Right! Student has been registered')
